a94953bb1ba7a76cf1fcab65fd1f3dd7a3965f4a
yzhao <yzhao@fortinet.com>
2022-01-11 09:43:32 -0800

    1

--- struct_example.go
+++ struct_example.go
@@ -8,9 +8,21 @@ type person struct {
 }
 
 func main(){
-	yi := person{"Yi", "Zhao"} // 不推荐这种写法，因为change the order of the field会导致出错
-	yi2 := person{firstName: "Yi2", lastName: "Zhao2"} // 推荐这种写法
+	{
+		yi := person{"Yi", "Zhao"} // 不推荐这种写法，因为change the order of the field会导致出错
+		fmt.Println(yi) // {Yi Zhao}
+	}
 
-	fmt.Println(yi) // {Yi Zhao}
-	fmt.Println(yi2) // {Yi2 Zhao2}
+	{
+		yi2 := person{firstName: "Yi2", lastName: "Zhao2"} // 推荐这种写法
+		fmt.Println(yi2) // {Yi2 Zhao2}
+	}
+
+	{
+		var yi3 person
+		yi3.firstName = "Yi3"
+		yi3.lastName = "Zhao3"
+		fmt.Println(yi3) // {Yi3 Zhao3}
+		fmt.Printf("%+v", yi3) // {firstName:Yi3 lastName:Zhao3}
+	}
 }
