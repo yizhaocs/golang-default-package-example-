b20014486084c146285d2f82ee578e7b7eddc508
yzhao <yzhao@fortinet.com>
2022-01-10 16:27:11 -0800

    1

--- error_handling_example.go
+++ error_handling_example.go
@@ -7,15 +7,16 @@ import (
 )
 
 func main()  {
-	error_example("xyz.txt")
+	error_example("write_to_filename.txt")
 }
 
 func error_example(filename string)  {
 	bytes, err := ioutil.ReadFile(filename)
-	if err != nil {
+	if err != nil { // 如果出错为nil则print出错误并且退出程序
 		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
 
 	fmt.Println("file found with content:", string(bytes))
 }
+
