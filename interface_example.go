f7c40f51d70783a3d64ce77e0b3eb1b3ae8008a1
yzhao <yzhao@fortinet.com>
2022-01-12 13:40:59 -0800

    1

--- interface_example.go
+++ interface_example.go
@@ -2,30 +2,30 @@ package main
 
 import "fmt"
 
+type bot interface {
+	getGreating() string
+}
+
 type englishBot struct{}
 type spanishBot struct{}
+
+func (eb englishBot) getGreating() string {
+	// VERY custom logic for generating an english greating
+	return "Hi There!"
+}
+
+func (sb spanishBot) getGreating() string {
+	return "Hola!"
+}
+
+func printGreating(b bot) {
+	fmt.Println(b.getGreating())
+}
 
 func main() {
 	eb := englishBot{}
 	sb := spanishBot{}
 
-	printGreating(eb)
-	printGreating(sb)
-}
-
-func printGreating(eb englishBot) {
-	fmt.Println(eb.getGreeting())
-}
-
-func printGreating(sb spanishBot) {
-	fmt.Println(sb.getGreeting())
-}
-
-func (eb englishBot) getGreeting() string {
-	// VERY custom logic for generating an english greating
-	return "Hi There!"
-}
-
-func (sb spanishBot) getGreeting() string {
-	return "Hola!"
+	printGreating(eb) // Hi There!
+	printGreating(sb) // Hola!
 }
