b455b25db021a118c2c3f22c2e48d50275c95a70
yzhao <yzhao@fortinet.com>
2022-01-13 13:43:43 -0800

    1

--- routine_thread/website_status_checker_go_routine.go
+++ routine_thread/website_status_checker_go_routine.go
@@ -27,14 +27,18 @@ func main() {
 		fmt.Println(i)
 		go checkLink_with_channel(link, channel)
 	}
+
+	fmt.Println(<- channel)
 }
 
 func checkLink_with_channel(link string, channel chan string) {
 	_, error := http.Get(link) // 不在乎response所以response改为下划线_，只保留error
 	if error != nil {
 		fmt.Println(link, "might be down!")
+		channel <- "Might be down I think"
 		return
 	}
 
 	fmt.Println(link, "is up!")
+	channel <- "Yep its up"
 }
