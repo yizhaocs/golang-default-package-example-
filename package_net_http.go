f2da21e187099f217505f5e44b57d84499344ebf
yzhao <yzhao@fortinet.com>
2022-01-13 10:39:26 -0800

    1

--- package_net_http.go
+++ package_net_http.go
@@ -7,6 +7,15 @@ import (
 	"os"
 )
 
+type logWriter struct{}
+
+// a custom implementation of the writer interface that still prints out all the data
+func (logWriter) Write(bs []byte) (int, error){
+	fmt.Println(string(bs))
+	fmt.Println("Just wrote this many bytes")
+	return len(bs), nil
+}
+
 func main() {
 	response, error := http.Get("http://google.com")
 	if error != nil {
@@ -17,23 +26,26 @@ func main() {
 	printResponseStatus(response)
 	printResponseBody_method1(response)
 	printResponseBody_method2(response)
-
-
+	printResponseBody_method3(response)
 }
 
-func printResponseStatus(response *http.Response){
+func printResponseStatus(response *http.Response) {
 	fmt.Println("response.Status:", response.Status) // response.Status: 200 OK
 }
 
-func printResponseBody_method1(response *http.Response){
+func printResponseBody_method1(response *http.Response) {
 	/*
 			Response: &{200 OK 200 HTTP/1.1 1 1 map[Cache-Control:[private, max-age=0] Content-Type:[text/html; charset=ISO-8859-1] Date:[Thu, 13 Jan 2022 18:01:27 GMT] Expires:[-1] P3p:[CP="This is not a P3P policy! See g.co/p3phelp for more info."] Server:[gws] Set-Cookie:[1P_JAR=2022-01-13-18; expires=Sat, 12-Feb-2022 18:01:27 GMT; path=/; domain=.google.com; Secure NID=511=hdnakqOzQCRYut8ZwUqa8r4pbPsmMAiXNxMAlqSCfJ4tN5VkXaSoYbcjcArEA6bwjyTtUZDAQUg19ZRjZ-OnZf1pK_IVl88w2YPxyaNqcyG3NluyGrRe3IgIP28d1uNefaREI2os7y9Ccsk1iXlpJ5LsIUllXJXxNsymdiFvmmU; expires=Fri, 15-Jul-2022 18:01:27 GMT; path=/; domain=.google.com; HttpOnly] X-Frame-Options:[SAMEORIGIN] X-Xss-Protection:[0]] 0xc0000be180 -1 [] false true map[] 0xc000216000 <nil>}
 		<!doctype html><html itemscope="" itemtype="http://schema.org/WebPage" lang="en"><head><meta content="Search the world's information, including webpages, images, videos and more. Google has many special features to help you find exactly what you're looking for." name="description"><meta content="noodp" name="robots"><meta content="text/html; charset=UTF-8" http-equiv="Content-Type"><meta content="/images/branding/googleg/1x/googleg_standard_color_128dp.png" itemprop="image"><title>Google</title>
 	*/
 	io.Copy(os.Stdout, response.Body)
 }
+func printResponseBody_method2( response *http.Response) {
+	lw := logWriter{}
+	io.Copy(lw, response.Body)
+}
 
-func printResponseBody_method2(response *http.Response){
+func printResponseBody_method3(response *http.Response) {
 	/*
 		So this make function right here is a built-In function in the language that takes a type of a slice.
 		And then as a second argument,
